33889fa67d58e5bddd3623d488a3f6a73d4c16d2
rego <rego@MacBook-Pro-Mac.local>
2018-12-12 03:06:10 +0300

    add relative routes

--- src/containers/switchRoute.jsx
+++ src/containers/switchRoute.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
-import {Switch, Route} from "react-router-dom";
-import {routes} from '../routes';
+import { Switch, Route, withRouter } from 'react-router-dom';
+import { routes } from '../routes';
 
-export const SwitchRoute = () => (
+const SwitchRouteInner = ({match}) => (
   <Switch>
     {
       routes.map((route) => {
         const Component = route.component;
+        // console.log('props', match.url + route.path);
         return (
           <Route 
-            key={route.path}
+            key={match.url + route.path}
             exact={route.isExact}
             path={route.path}
-            render={(props) => <Component {...props} redirect={route.redirect} innerComponent={route.innerComponent} />}
+            render={(props) => console.log(props) || <Component {...props} redirect={route.redirect} innerComponent={route.innerComponent} />}
           />
         );
       })
     }
   </Switch>
 );
+
+export const  SwitchRoute = withRouter(SwitchRouteInner);
