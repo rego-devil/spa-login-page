5765743fe3289bba80a71f5c855e6e6f37b29160
rego <rego@MacBook-Pro-Mac.local>
2018-12-12 03:20:32 +0300

    relative paths

--- src/containers/switchRoute.jsx
+++ src/containers/switchRoute.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
 import { routes } from '../routes';
 
-const SwitchRouteInner = ({match}) => (
+const SwitchRouteInner = (props) => (
+  console.log('props', props) || 
   <Switch>
     {
       routes.map((route) => {
         const Component = route.component;
-        console.log('props', match.url + route.path);
+        console.log('props', props.match.url + route.path);
         return (
           <Route 
-            key={match.url + route.path}
+            key={props.match.url + route.path}
             exact={route.isExact}
             path={route.path}
-            render={(props) => console.log(props) || <Component {...props} redirect={route.redirect} innerComponent={route.innerComponent} />}
+            render={(props) => <Component {...props} redirect={route.redirect} innerComponent={route.innerComponent} />}
           />
         );
       })
